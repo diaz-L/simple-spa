d7514fc6e5cf16946eb03c507542211ba3bd9c08
diaz-L <lediar.a.diaz@gmail.com>
2018-12-27 21:21:07 -0500

    simple app using react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom'
+import Home from './Home'
+import Stuff from './Stuff'
+import Contact from './Contact'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<div>
+				<h1 className='title'>Simple SPA</h1>
+				<ul>
+					<li><NavLink exact to='/'>Home</NavLink></li>
+					<li><NavLink to='/stuff'>Stuff</NavLink></li>
+					<li><NavLink to='/contact'>Contact</NavLink></li>
+				</ul>
+				<div className='content'>
+					<Route exact path='/' component={Home} />
+					<Route path='/stuff' component={Stuff} />
+					<Route path='/Contact' component={Contact} />
+				</div>
+				</div>
+			</Router>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+	return (
+		<div>
+			<h1>Contact</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <a href='#'>veniam</a>.</p>
+		</div>
+	)
+}
+
+export default Contact
